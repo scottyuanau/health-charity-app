e69c4e36728ef00c720224bd19bd27c2906e0389
Scott Yuan <scoworthyuan@gmail.com>
2025-10-10 21:35:35 +1100

    extracted navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NaviBar from '@/components/NaviBar.vue'
 
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NaviBar />
   </header>
 
   <RouterView />
---
+++ src/components/NaviBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import MenuBar from 'primevue/menubar'
+
+const menuItems = [
+  { label: 'Home', to: '/' },
+  { label: 'About', to: '/about' }
+]
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom shadow-sm">
+    <div class="container">
+      <MenuBar :model="menuItems" class="flex-grow-1 border-0 bg-transparent">
+        <template #start>
+          <RouterLink
+            class="navbar-brand fw-semibold text-decoration-none text-body"
+            to="/"
+          >
+            Health Charity
+          </RouterLink>
+        </template>
+      </MenuBar>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
